48da15ef54d83a0b414f79bd2af2bb35cb34c2c9
Segun <adebanjosegun589@gmail.com>
2019-09-23 05:26:48 +0100

    improves UI

--- src/components/votesContainer/index.js
+++ src/components/votesContainer/index.js
@@ -113,45 +113,48 @@ const Votes = () => {
     };
 
       return(
-          <div>
-            <form>
-                <div className="form-group">
+          <div className="container text-center">
+            <form className="voter-form col-md-12 margin-btm">
+                <div className="form-col row">
+                  <div className=" col-md-6">
                     <label htmlFor="votersName">Voters Name:</label>
-                    <select onChange={handleCardNo}>
-                            <option>[----select Voter----]</option>
+                    <select onChange={handleCardNo} className="margin-btm">
+                            <option>[-select Voter-]</option>
                             {Voters.map(vote => (
                               <option value={vote.voter_card_no} key={vote.voter_card_no}>
                                 {vote.voter_name}
                               </option>
                             ))}
                     </select>
-
+                  </div>
+                  <div className="col-md-6">
                     <label htmlFor="votersCardNo">Select Party</label>
-                    <select onChange={handleParty}>
-                            <option>[----select Party----]</option>
+                    <select onChange={handleParty} className="margin-btm">
+                            <option>[-select Party-]</option>
                             {Parties.map(party => (
                               <option value={party.party} key={party.id}>
                                 {party.party}
                               </option>
                             ))}
                     </select>
+                  </div>
                     <button
                       type="submit"
-                      className="btn btn-primary"
+                      className="btn btn-primary margin-btm mx-auto"
                       onClick={submission}
                     >
                       Submit
                     </button>
                 </div>
             </form>
-            <div className="vote_count">
-              <div className="apc_vote_count">
+            <div className="vote_count row">
+              <div className="apc_vote_count col-md-5 shadow-sm m-4">
                 <label htmlFor="apc_vote_count">APC:</label>
-                <div>{apcVote.length}</div>
+                <div className="font-anton">{apcVote.length}</div>
               </div>
-              <div className="apc_vote_count">
-                <label htmlFor="apc_vote_count">PDP:</label>
-                <div>{pdpVote.length}</div>
+              <div className="pdp_vote_count col-md-5 shadow-sm m-4">
+                <label htmlFor="pdp_vote_count">PDP:</label>
+                <div className="font-anton">{pdpVote.length}</div>
               </div>
             </div>
           </div>
